288cc75121de407794e11f4bc466276e1c6f0ce7
SakalNatalia <sakalnatalia101@gmail.com>
2025-04-25 17:15:42 +0200

    noticed font size error

--- marriageweb/src/components/FooTer.vue
+++ marriageweb/src/components/FooTer.vue
@@ -3,7 +3,7 @@
       <div class="overlay"></div>
       <div class="container1 text-center text-white position-relative py-5">
         <h2 class="display-4 mt-5">Thank</h2>
-        <h2>you</h2>
+        <h2 class="big">you</h2>
         <p class="lead mt-5">Albert & Emily</p>
       </div>
     </section>
